0f492108b10bf9bbbb2ef05eac5076c581649aea
Corina Allen <StarPerfect@gmail.com>
2019-07-30 09:41:19 -0600

    Iteration Three up to Pantry#Restock complete

---
+++ lib/pantry.rb
@@ -0,0 +1,15 @@
+class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def restock(item, quantity)
+    @stock[item] += quantity
+  end
+end
---
+++ test/pantry_test.rb
@@ -0,0 +1,33 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+  end
+
+  def test_pantry_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_attributes
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_stock_check_and_restock
+    assert_equal 0, @pantry.stock_check(@cheese)
+
+    @pantry.restock(@cheese, 5)
+    @pantry.restock(@cheese, 10)
+
+    assert_equal 15, @pantry.stock_check(@cheese)
+  end
+end

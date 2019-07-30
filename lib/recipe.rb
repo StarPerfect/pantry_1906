6a25474393f580b3f95a963711e2aeb2460cadfa
Corina Allen <StarPerfect@gmail.com>
2019-07-30 09:29:04 -0600

    Iteration Two complete up to Ingredients method - still need to do total_calories

---
+++ lib/recipe.rb
@@ -0,0 +1,20 @@
+class Recipe
+  attr_reader :name, :ingredients_required
+
+  def initialize(name)
+    @name = name
+    @ingredients_required = Hash.new
+  end
+
+  def add_ingredient(item, quantity)
+    @ingredients_required[item] = quantity
+  end
+
+  def amount_required(item)
+    @ingredients_required[item]
+  end
+
+  def ingredients
+    @ingredients_required.keys
+  end
+end
